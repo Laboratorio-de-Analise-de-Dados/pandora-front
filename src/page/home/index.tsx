6c2b2cc96e1d91fa20bd4488212723520f61a99b
Paulo Moro <paulo.morolol@gmail.com>
2024-04-07 15:05:42 -0300

    feat: create a home page with initial info

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -1,7 +1,9 @@
 // import { useEffect, useState } from "react";
-import { Box } from '@mui/material';
+import { Box, Button, IconButton, Typography } from '@mui/material';
 import fcsData from '../../assets/FCS-1707477508554.json';
-import ScatterPlot from "../../components/plotly";
+// import ScatterPlot from "../../components/plotly";
+import Layout from '../../components/Layout';
+import { Add } from '@mui/icons-material';
 export type EventData ={ 
   id:number
   value:number
@@ -12,10 +14,24 @@ export default function HomePage(){
 
     const data = fcsData.values.map(data=>({id: data.id, x: data.fsc_a, y:data.ssc_a}))
     return (
-      <Box sx={{ flex: 1, padding: '1rem' }}>
-        <Box sx={{padding:'1rem'}}>
-          <ScatterPlot data={data} maxItems={10000} chunkSize={1000} xAxisSelector='fsc_a' yAxisSelector='ssc_a' />
-        </Box>
-      </Box>
+      <Layout>
+          <Typography sx={{fontSize:'2rem', fontWeight:'bold'}}>
+              Welcome to CitoSharp!
+          </Typography>
+
+
+          <Box>
+            <Typography sx={{fontSize:'1.5rem'}}>
+              Please make your choice:
+            </Typography>
+            <Box>
+              <IconButton><Add/></IconButton>
+              <IconButton></IconButton>
+            </Box>
+          </Box>
+
+          {/* <ScatterPlot data={data} maxItems={10000} chunkSize={1000} xAxisSelector='fsc_a' yAxisSelector='ssc_a' /> */}
+      
+      </Layout>
     )
 }
