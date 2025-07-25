ea872d324ef4de0529187d433d7445a14c4f5114
Paulo Moro <paulo.morolol@gmail.com>
2025-07-25 01:16:59 -0300

    refactor: linter changes on layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,16 +1,19 @@
-import { Box } from "@mui/material";
-import { ReactNode } from "react";
+import { Box } from "@mui/material"
+import { ReactNode } from "react"
 
 interface LayoutPropsInterface {
-  children:ReactNode
+	children: ReactNode
 }
 
-export default function Layout({children}:LayoutPropsInterface){
-  return(
-    <Box sx={{ flex: 1, padding: '1rem' }}>
-      {children}
-    </Box>
-  )
-
-
+export default function Layout({ children }: LayoutPropsInterface) {
+	return (
+		<Box
+			sx={{
+				display: "flex",
+				flex: 1,
+			}}
+		>
+			{children}
+		</Box>
+	)
 }
