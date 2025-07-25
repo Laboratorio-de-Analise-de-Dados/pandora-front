64c794e25c4a36a33cd766593d5afdeba2b514a4
Paulo Moro <paulo.morolol@gmail.com>
2025-07-25 01:17:40 -0300

    refactor: change data grid to tree on file and gate render

--- src/components/page/experiment/[id]/index.tsx
+++ src/components/page/experiment/[id]/index.tsx
@@ -1,11 +1,12 @@
 import { Box, CircularProgress, Typography } from "@mui/material"
-import { DataGrid, GridColDef, GridEventListener } from "@mui/x-data-grid"
 import { useCallback, useEffect, useState } from "react"
+import Layout from "../../../Layout"
 import { useParams } from "react-router-dom"
 import { toast } from "react-toastify"
 import CytometryApi from "../../../../API"
 import { Experiment, ExperimentFiles, FileData } from "../../../../types"
 import ScatterPlot from "../../../plotly"
+import ParentTree from "../../../parent_tree"
 interface Params {
 	id: string
 }
@@ -33,79 +34,59 @@ export default function ExperimentPage() {
 		}
 	}, [])
 
-	const columns: GridColDef[] = [
-		{ field: "id", headerName: "id" },
-		{ field: "file_name", headerName: "file name", width: 500 },
-	]
-
 	useEffect(() => {
 		getExperimentData(param.id)
 	}, [param.id, getExperimentData])
 
-	const handleRowClick: GridEventListener<"rowClick"> = async (item) => {
-		const fileId = item.row.id
-		setLoadFile(true)
-		try {
-			const fileData = await CytometryApi.get(
-				`/experiment/file/${fileId}/list?limit=10000`
-			)
-			setFileData(fileData.data)
-		} catch (error: any) {
-			toast.error(error.message)
-		} finally {
-			setLoadFile(false)
-		}
-	}
-
 	return (
-		<Box
-			sx={{
-				display: "flex",
-				flexDirection: "column",
-				gap: "2rem",
-				justifyContent: "space-around",
-				padding: "2rem",
-				height: "80vh",
-			}}
-		>
-			<Box>
-				<Typography>{experiment?.title}</Typography>
-			</Box>
-			<Box sx={{ height: "70%", display: "flex", gap: "2rem" }}>
-				<DataGrid
-					columns={columns}
-					rows={experimentFiles}
-					loading={loading}
-					onRowClick={handleRowClick}
-				></DataGrid>
-				<Box
-					sx={{
-						width: "50%",
-						display: "flex",
-						flexDirection: "column",
-						alignItems: "center",
-						justifyContent: "center",
-						gap: "2rem",
-					}}
+		<Layout>
+			<Box
+				sx={(theme) => ({
+					padding: "1rem",
+					bgcolor: theme.palette.background.default,
+					width: "20%",
+				})}
+			>
+				<Typography
+					sx={(theme) => ({
+						color: theme.palette.text.primary,
+					})}
 				>
-					{loadFile && <CircularProgress />}
-					{!fileData && !loadFile && (
-						<Box>
-							<Typography>Select a file to load</Typography>
-						</Box>
-					)}
-					{!loadFile && fileData?.data_set && (
-						<>
-							<Typography>{fileData.file_name}</Typography>
-							<ScatterPlot
-								data={fileData.data_set}
-								loading={loading}
-								values={experiment?.values || []}
-							/>
-						</>
-					)}
-				</Box>
+					{experiment?.title}
+				</Typography>
+				<ParentTree
+					files={experimentFiles}
+					fileDataSet={setFileData}
+					loadFile={setLoadFile}
+				/>
 			</Box>
-		</Box>
+			<Box
+				sx={{
+					display: "flex",
+					justifyContent: "center",
+					flex: 2,
+					alignItems: "center",
+					flexDirection: "column",
+					gap: "1rem",
+				}}
+			>
+				{loadFile && <CircularProgress />}
+				{!fileData && !loadFile && (
+					<Box>
+						<Typography>Select a file to load</Typography>
+					</Box>
+				)}
+				{!loadFile && fileData?.data_set && (
+					<>
+						<Typography>{fileData.file_name}</Typography>
+						<ScatterPlot
+							data={fileData.data_set}
+							loading={loading}
+							values={experiment?.values || []}
+						/>
+					</>
+				)}
+			</Box>
+		</Layout>
 	)
 }
