d89f1be9d30efd94a50bf881320a774016fb1dfe
Paulo Moro <paulo.morolol@gmail.com>
2025-07-25 01:19:47 -0300

    refactor: add gate do each filedata config

--- src/types/ExperimentTypes.ts
+++ src/types/ExperimentTypes.ts
@@ -1,18 +1,28 @@
 export interface Experiment {
-  id:number
-  title: string
-  type: string
-  values: string[]
-  active: boolean
+	id: number
+	title: string
+	type: string
+	values: string[]
+	active: boolean
 }
 
 export interface ExperimentFiles {
-  id:number
-  file_name:string
+	id: number
+	file_name: string
+	gates: Gate[]
 }
 
 export interface FileData {
-  id: number
-  file_name: string
-  data_set:any
+	id: number
+	file_name: string
+	data_set: any
+	gates: Gate[]
+}
+
+export interface Gate {
+	id: number
+	name: string
+	gate_coordinates: object
+	dashboard?: object
+	children?: Gate[]
 }
