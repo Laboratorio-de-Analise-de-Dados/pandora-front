3f92f72986c424949ff04ae579d75b8fac50cc41
Paulo Moro <paulo.morolol@gmail.com>
2025-10-30 12:11:28 -0300

    feat: add deploy config

--- src/API/index.ts
+++ src/API/index.ts
@@ -1,5 +1,6 @@
-import axios from 'axios';
+import axios from "axios"
 
-const CytometryApi = axios.create({baseURL:'http://localhost:8000'})
+const pandoraUrl = process.env.API_URL || "http://localhost:8085"
+const CytometryApi = axios.create({ baseURL: pandoraUrl })
 
 export default CytometryApi
