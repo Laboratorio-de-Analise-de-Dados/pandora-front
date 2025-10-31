788d4f7b4f529ad39788fa4dc8dcfb995bb963eb
Paulo Moro <paulo.morolol@gmail.com>
2025-10-31 10:45:08 -0300

    refactor: change port and add fixed api endpoint

--- src/API/index.ts
+++ src/API/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const pandoraUrl = process.env.API_URL || "http://localhost:8085"
+const pandoraUrl = process.env.API_URL || "http://164.92.239.48:8085"
 const CytometryApi = axios.create({ baseURL: pandoraUrl })
 
 export default CytometryApi
