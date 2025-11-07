9cee400f27a8fff8636ed85ea6abfc2745bb1a5b
Paulo Moro <paulo.morolol@gmail.com>
2025-11-07 13:57:08 -0300

    refactor: uses api.project-pandora url

--- src/API/index.ts
+++ src/API/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
-// const pandoraUrl = process.env.API_URL || "https://api.project-pandora.com.br"
-const pandoraUrl = process.env.API_URL || "http://localhost:8085"
+const pandoraUrl = process.env.API_URL || "https://api.project-pandora.com.br"
+// const pandoraUrl = process.env.API_URL || "http://localhost:8085"
 const CytometryApi = axios.create({ baseURL: pandoraUrl })
 
 export default CytometryApi
